c8fb458cb63328a7b444b583ca73d5a166ddf592
Gurdip Singh <singh.gurdip24@gmail.com>
2020-06-12 23:35:51 -0400

    removed duplicate code

--- src/util/APIUtils.js
+++ src/util/APIUtils.js
@@ -33,22 +33,6 @@ export function getAllPolls(page, size) {
     });
 }
 
-export function createPoll(pollData) {
-    return request({
-        url: API_BASE_URL + "/polls",
-        method: 'POST',
-        body: JSON.stringify(pollData)         
-    });
-}
-
-export function castVote(voteData) {
-    return request({
-        url: API_BASE_URL + "/polls/" + voteData.pollId + "/votes",
-        method: 'POST',
-        body: JSON.stringify(voteData)
-    });
-}
-
 export function login(loginRequest) {
     return request({
         url: API_BASE_URL + "/auth/login",
@@ -98,22 +82,12 @@ export function getUserProfile(username) {
     });
 }
 
-export function getUserCreatedPolls(username, page, size) {
+export function getUserSavedArticles(username, page, size) {
     page = page || 0;
     size = size || POLL_LIST_SIZE;
 
     return request({
         url: API_BASE_URL + "/users/" + username + "/polls?page=" + page + "&size=" + size,
-        method: 'GET'
-    });
-}
-
-export function getUserVotedPolls(username, page, size) {
-    page = page || 0;
-    size = size || POLL_LIST_SIZE;
-
-    return request({
-        url: API_BASE_URL + "/users/" + username + "/votes?page=" + page + "&size=" + size,
         method: 'GET'
     });
 }
