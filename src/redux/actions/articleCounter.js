5d2aefc5e0b3eab4a11c12207858af66032c5b0c
Gurdip Singh <singh.gurdip24@gmail.com>
2020-06-25 23:52:18 -0400

    redux code boilerplate

---
+++ src/redux/actions/articleCounter.js
@@ -0,0 +1,5 @@
+const articleCounter = (state = 0, action) => {
+    return state;
+}
+
+export default articleCounter
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import articleCounter from '../actions/articleCounter';
+
+const rootReducer = combineReducers({
+    articleCounter
+})
+
+export default rootReducer
