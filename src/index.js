13c263d4bb9bf559862935446feb5724ed1a08fc
Gurdip Singh <singh.gurdip24@gmail.com>
2020-06-25 23:57:51 -0400

    conflicts resolved

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {ReactDOM, render} from 'react-dom';
 import './index.css';
 import App from './app/App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+import configureStore from './redux/store/configureStore';
+
+const store = configureStore();
+
 ReactDOM.render(
+    <Provider store={store}>
     <Router>
         <App />
-    </Router>, 
+    </Router>
+    </Provider>, 
     document.getElementById('root')
 );
 
